a04f1f06fbbc962cf4e7157589d77f9df6cab114
Seunghyuk Choi <noaidi@gmail.com>
2024-12-26 13:55:33 +0900

    background color for our values

---
+++ brochure/assets/immutable/entry/start.GGzgppbR.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.Dths3ytA.js";export{t as start};
---
+++ brochure/assets/immutable/nodes/2.Bb57E02C.js
@@ -0,0 +1,1 @@
+import"../chunks/disclose-version.C0T-MG5w.js";import"../chunks/legacy.a8nwY6Kj.js";import{P as p}from"../chunks/index.0E5URTlT.js";function a(o){p(o)}export{a as component};
